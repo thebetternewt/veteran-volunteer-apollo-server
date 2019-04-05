7e129cf4e875484063c85719599cbbe9bd34aed4
Chris Eady <chris.eady1@gmail.com>
2019-04-04 21:37:25 -0500

    Add nodemon.json for nodemon config (ignore client files)

--- client/src/components/volunteerProfile/VolunteerProfileForm.js
+++ client/src/components/volunteerProfile/VolunteerProfileForm.js
@@ -1,10 +1,11 @@
 import { Button, Checkbox, Form, Input } from 'antd'
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Mutation } from 'react-apollo'
 import {
   CREATE_VOLUNTEER_PROFILE,
   UPDATE_VOLUNTEER_PROFILE,
 } from '../../apollo/mutations'
+import { availabilityOptions } from '../../constants'
 import graphQlErrors from '../../util/graphqlErrors'
 import { hasErrors } from '../common/forms/helpers'
 import PlaceSearchField from '../common/forms/PlaceSearchField'
@@ -22,79 +23,74 @@ const contactOptions = ['Phone', 'Email']
 //   return Object.keys(fieldsErrors).some(field => fieldsErrors[field])
 // }
 
-class VolunteerProfileForm extends Component {
-  state = {
-    confirmDirty: false,
-    indeterminate: false,
-    checkAll: false,
-    location: {
-      lat: null,
-      lng: null,
-    },
-    searchResults: [],
-    selectedLocation: null,
+const VolunteerProfileForm = props => {
+  const [confirmDirty, setConfirmDirty] = useState(false)
+  const [availability, setAvailability] = useState([])
+  const [availabilityDetail, setAvailabilityDetail] = useState('')
+  const [location, setLocation] = useState({ lat: null, lng: null })
+  const [searchResults, setSearchResults] = useState([])
+  const [selectedLocation, setSelectedLocation] = useState(null)
+
+  const { profile, form, toggleForm } = props
+
+  // getInitState = () => {
+
+  //   const checkedList = []
+  //   if (profile) {
+  //     if (profile.allowEmailContact) {
+  //       checkedList.push('Email')
+  //     }
+  //     if (profile.allowPhoneContact) {
+  //       checkedList.push('Phone')
+  //     }
+
+  //     if (profile.location) {
+  //       this.setState({
+  //         location: { lat: profile.location[1], lng: profile.location[0] },
+  //       })
+  //     }
+
+  //     form.setFieldsValue({ preferredContact: checkedList })
+  //     this.handleChecklistChange(checkedList)
+  //   }
+  // }
+
+  // componentDidMount() {
+  //   this.getInitState()
+  // }
+
+  const handleChecklistChange = checkedList => {
+    setAvailability(checkedList)
+    // this.setState({
+    //   checkedList,
+    //   indeterminate:
+    //     !!checkedList.length && checkedList.length < contactOptions.length,
+    //   checkAll: checkedList.length === contactOptions.length,
+    // })
   }
 
-  getInitState = () => {
-    const { profile, form } = this.props
+  // const onCheckAllChange = e => {
+  //   const { setFieldsValue } = form
+  //   if (e.target.checked) {
+  //     setFieldsValue({ preferredContact: contactOptions })
+  //   } else {
+  //     setFieldsValue({ preferredContact: [] })
+  //   }
+  //   this.setState({
+  //     // checkedList: e.target.checked ? contactOptions : [],
+  //     indeterminate: false,
+  //     checkAll: e.target.checked,
+  //   })
+  // }
 
-    const checkedList = []
-    if (profile) {
-      if (profile.allowEmailContact) {
-        checkedList.push('Email')
-      }
-      if (profile.allowPhoneContact) {
-        checkedList.push('Phone')
-      }
+  const handleLocationSelect = newLocation => setLocation(newLocation)
 
-      if (profile.location) {
-        this.setState({
-          location: { lat: profile.location[1], lng: profile.location[0] },
-        })
-      }
-
-      form.setFieldsValue({ preferredContact: checkedList })
-      this.handleChecklistChange(checkedList)
-    }
-  }
-
-  componentDidMount() {
-    this.getInitState()
-  }
-
-  handleChecklistChange = checkedList => {
-    this.setState({
-      checkedList,
-      indeterminate:
-        !!checkedList.length && checkedList.length < contactOptions.length,
-      checkAll: checkedList.length === contactOptions.length,
-    })
-  }
-
-  onCheckAllChange = e => {
-    const { setFieldsValue } = this.props.form
-    if (e.target.checked) {
-      setFieldsValue({ preferredContact: contactOptions })
-    } else {
-      setFieldsValue({ preferredContact: [] })
-    }
-    this.setState({
-      // checkedList: e.target.checked ? contactOptions : [],
-      indeterminate: false,
-      checkAll: e.target.checked,
-    })
-  }
-
-  handleLocationSelect = location =>
-    this.setState({ selectedLocation: location })
-
-  handleSubmit = async (e, submit) => {
+  const handleSubmit = async (e, submit) => {
     e.preventDefault()
-    const { validateFields } = this.props.form
+    const { validateFields } = form
 
     await validateFields(async (errors, values) => {
       console.log('errors:', errors)
-      const { selectedLocation } = this.state
       if (!errors) {
         const variables = {
           allowPhoneContact: values.preferredContact.includes('Phone'),
@@ -117,7 +113,7 @@ class VolunteerProfileForm extends Component {
            * point, the toggleForm function no longer exists and throws an
            * error in the console. There may be a better way to handle this.
            */
-          this.props.toggleForm && this.props.toggleForm()
+          toggleForm && toggleForm()
         } catch (err) {
           console.error(err)
         }
@@ -126,103 +122,101 @@ class VolunteerProfileForm extends Component {
     })
   }
 
-  render() {
-    const { profile, toggleForm, form } = this.props
-    const { getFieldDecorator, getFieldsError } = form
+  // const { profile, toggleForm, form } = this.props
+  const { getFieldDecorator, getFieldsError } = form
 
-    console.log(toggleForm)
+  console.log(toggleForm)
 
-    const profileForm = ({ error, loading, submit }) => (
-      <Form onSubmit={e => this.handleSubmit(e, submit)}>
-        <Form.Item>{error && graphQlErrors(error)}</Form.Item>
+  const profileForm = ({ error, loading, submit }) => (
+    <Form onSubmit={e => handleSubmit(e, submit)}>
+      <Form.Item>{error && graphQlErrors(error)}</Form.Item>
 
-        <Form.Item label="Bio">
-          {getFieldDecorator('bio', {
-            rules: [
-              {
-                required: true,
-                min: 100,
-                max: 500,
-                message:
-                  'Please enter a short description of yourself between 100 and 500 characters.',
-              },
-            ],
-          })(<Input.TextArea autosize />)}
-        </Form.Item>
+      <Form.Item label="Bio">
+        {getFieldDecorator('bio', {
+          rules: [
+            {
+              required: true,
+              min: 100,
+              max: 500,
+              message:
+                'Please enter a short description of yourself between 100 and 500 characters.',
+            },
+          ],
+        })(<Input.TextArea autosize />)}
+      </Form.Item>
 
-        <Form.Item label="Preferred Contact">
-          <Checkbox
+      <Form.Item label="Preferred Contact">
+        {/* <Checkbox
             indeterminate={this.state.indeterminate}
             onChange={this.onCheckAllChange}
             checked={this.state.checkAll}
           >
             Check all
-          </Checkbox>
+          </Checkbox> */}
 
-          <hr />
+        <hr />
 
-          {getFieldDecorator('preferredContact', {
-            rules: [
-              {
-                required: true,
-                message: 'Please choose at least one preferred contact method.',
-              },
-            ],
-          })(
-            <CheckboxGroup
-              options={contactOptions}
-              onChange={this.handleChecklistChange}
-            />
-          )}
-        </Form.Item>
-
-        {!profile && (
-          <PlaceSearchField
-            fieldname="location"
-            label="Where are you located?"
-            form={form}
-            setLocationState={this.handleLocationSelect}
+        {getFieldDecorator('preferredContact', {
+          rules: [
+            {
+              required: true,
+              message: 'Please choose at least one preferred contact method.',
+            },
+          ],
+        })(
+          <CheckboxGroup
+            options={availabilityOptions.map(opt => opt.name)}
+            onChange={handleChecklistChange}
           />
         )}
+      </Form.Item>
 
-        <Form.Item>
-          <Button
-            type="primary"
-            htmlType="submit"
-            disabled={hasErrors(getFieldsError())}
-            loading={loading}
-            style={{ marginRight: '2rem' }}
-          >
-            {profile ? 'Update Profile' : 'Create Profile'}
+      {!profile && (
+        <PlaceSearchField
+          fieldname="location"
+          label="Where are you located?"
+          form={form}
+          setLocationState={handleLocationSelect}
+        />
+      )}
+
+      <Form.Item>
+        <Button
+          type="primary"
+          htmlType="submit"
+          disabled={hasErrors(getFieldsError())}
+          loading={loading}
+          style={{ marginRight: '2rem' }}
+        >
+          {profile ? 'Update Profile' : 'Create Profile'}
+        </Button>
+        {profile && !loading && (
+          <Button type="secondary" onClick={toggleForm}>
+            Cancel
           </Button>
-          {profile && !loading && (
-            <Button type="secondary" onClick={toggleForm}>
-              Cancel
-            </Button>
-          )}
-        </Form.Item>
-      </Form>
-    )
-
-    return (
-      <>
-        <h1>{profile ? 'Update Profile' : 'Create Volunteer Profile'}</h1>
-        {profile ? (
-          <Mutation mutation={UPDATE_VOLUNTEER_PROFILE}>
-            {(updateProfile, { loading, error }) =>
-              profileForm({ error, loading, submit: updateProfile })
-            }
-          </Mutation>
-        ) : (
-          <Mutation mutation={CREATE_VOLUNTEER_PROFILE}>
-            {(createProfile, { loading, error }) =>
-              profileForm({ error, loading, submit: createProfile })
-            }
-          </Mutation>
         )}
-      </>
-    )
-  }
+      </Form.Item>
+    </Form>
+  )
+
+  return (
+    <>
+      <h1>{profile ? 'Update Profile' : 'Create Volunteer Profile'}</h1>
+      {profile ? (
+        <Mutation mutation={UPDATE_VOLUNTEER_PROFILE}>
+          {(updateProfile, { loading, error }) =>
+            profileForm({ error, loading, submit: updateProfile })
+          }
+        </Mutation>
+      ) : (
+        <Mutation mutation={CREATE_VOLUNTEER_PROFILE}>
+          {(createProfile, { loading, error }) =>
+            profileForm({ error, loading, submit: createProfile })
+          }
+        </Mutation>
+      )}
+    </>
+  )
 }
 
 export default Form.create({ name: 'volunteerProfile' })(VolunteerProfileForm)
---
+++ client/src/constants.js
@@ -0,0 +1,28 @@
+export const needTypes = {
+  travel: {
+    option: 'TRAVEL',
+    name: 'Travel',
+    icon: 'car',
+  },
+  childcare: {
+    option: 'CHILDCARE',
+    name: 'Childcare',
+    icon: 'smile',
+  },
+  lawncare: {
+    option: 'LAWNCARE',
+    name: 'Lawncare',
+    icon: 'home',
+  },
+}
+
+export const availabilityOptions = [
+  {
+    option: 'WEEKDAYS',
+    name: 'Weekdays',
+  },
+  {
+    option: 'WEEKENDS',
+    name: 'Weekends',
+  },
+]
