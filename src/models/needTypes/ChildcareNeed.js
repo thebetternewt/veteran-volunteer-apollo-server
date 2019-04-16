a1743f519e7e3dbef8da4888a97b9c94edca3e7f
Chris Eady <chris.eady1@gmail.com>
2019-04-16 12:47:44 -0500

    Add corrected filenames

---
+++ src/models/needTypes/ChildcareNeed.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose'
+import 'mongoose-geojson-schema'
+
+const ages = ['INFANT', 'TODDLER', 'CHILD', 'TEENAGER']
+
+const {
+  Schema,
+  Types: { ObjectId, Point },
+} = mongoose
+
+const schema = new Schema(
+  {
+    needType: {
+      type: String,
+      default: 'CHILDCARE',
+    },
+    age: {
+      type: [String],
+      required: true,
+    },
+    need: {
+      type: ObjectId,
+      ref: 'Need',
+      required: true,
+    },
+    description: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+export default mongoose.model('ChildcareNeed', schema)
---
+++ src/models/needTypes/TravelNeed.js
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose'
+import 'mongoose-geojson-schema'
+
+const {
+  Schema,
+  Types: { ObjectId, Point },
+} = mongoose
+
+const schema = new Schema(
+  {
+    needType: {
+      type: String,
+      default: 'TRAVEL',
+    },
+
+    fromLocation: {
+      address: {
+        type: String,
+        required: true,
+      },
+      point: {
+        type: Point,
+        required: true,
+      },
+    },
+    toLocation: {
+      address: {
+        type: String,
+        required: true,
+      },
+      point: {
+        type: Point,
+        required: true,
+      },
+    },
+    need: {
+      type: ObjectId,
+      ref: 'Need',
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+export default mongoose.model('TravelNeed', schema)
